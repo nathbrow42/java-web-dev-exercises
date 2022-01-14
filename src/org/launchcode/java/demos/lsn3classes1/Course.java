eaccf24eb334dec36b0000f4c45ae072b58be6b8
Nathalie B <37987956+nathbrow42@users.noreply.github.com>
2022-01-13 19:15:31 -0500

    finished(?) less3 exercises

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,13 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+
+import org.launchcode.java.demos.lsn4classes2.Student;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String className;
+    private String code;
+    private int numberOfCredits;
+    private ArrayList<Student> roster;
+}
